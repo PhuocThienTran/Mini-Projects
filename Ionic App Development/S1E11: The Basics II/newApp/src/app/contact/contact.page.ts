62882ca1bda6ebdf869736f597af73d8ff985f93
Phuoc Thien Tran <67044495+PhuocThienTran@users.noreply.github.com>
2021-07-01 13:13:04 +1000

    Add files via upload

---
+++ Ionic App Development/S1E11: The Basics II/newApp/src/app/contact/contact.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './contact.page.html',
+  styleUrls: ['./contact.page.scss'],
+})
+export class ContactPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

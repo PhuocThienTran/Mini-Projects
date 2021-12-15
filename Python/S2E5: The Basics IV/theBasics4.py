46832166b5fc9cd5814b56c0415d75013f689738
Phuoc Thien Tran <67044495+PhuocThienTran@users.noreply.github.com>
2021-12-15 19:08:14 +1000

    Create theBasics4.py

---
+++ Python/S2E5: The Basics IV/theBasics4.py
@@ -0,0 +1,15 @@
+# Thien Tran Programming Series
+# Season 2 Episode 5: The Python’s Basics IV
+# YouTube Link: TBD
+
+# Types of Errors:
+    # Syntax: Problems detected as program is parsed, due to arrangement of symbols
+    # Logical Semantic: Errors made by a programmer
+    # Unavoidable: Detected at run time
+
+# Basic File Syntax:
+    # aFile = open(path, mode="r"): r: read current file from the path
+    # aFile = read(): read the entire file at once
+    # aFile.readline(): Read a single line, newline (\n) appears at the end if the file ends with a newline
+    # aFile.close(): always close the file at the end after opening it.
+
